3ccbb8f0364af185ecd91664d50e501110cd2853
YuyangZhang <yuyangzhang2025@gmail.com>
2025-03-29 21:26:35 +0800

    Add Shakespeare dataloader

--- data/shakespear/prepare-shakespeare.py
+++ data/shakespear/prepare-shakespeare.py
@@ -6,44 +6,58 @@ import numpy as np
 DATA_URL = "https://raw.githubusercontent.com/karpathy/char-rnn/master/data/tinyshakespeare/input.txt"
 
 
-def download_file():
-    input_file_path = os.path.join(os.path.dirname(__file__), "input.txt")
+def download_and_tokenize_shakespeare():
+    input_file_path = os.path.join(os.path.dirname(__file__), "shakespear.txt")
+
     if not os.path.exists(input_file_path):
         with open(input_file_path, "w") as f:
             f.write(requests.get(DATA_URL).text)
+    print("Shakespeare dataset downloaded.")
 
-    return input_file_path
-
-
-def load_and_tokenize(input_file_path):
-    if not os.path.exists(input_file_path):
-        input_file_path = download_file()
-
-    with open(input_file_path, "r", encoding="utf-8") as f:
+    with open(input_file_path, "r") as f:
         data = f.read()
 
-    n = len(data)
-    train_data = data[: int(n * 0.9)]
-    val_data = data[int(n * 0.9) :]
+    print("Shakespeare dataset loaded.")
 
-    # Encode Text
+    n = len(data)
+
+    train_data = data[: int(0.9 * n)]
+    val_data = data[int(0.9 * n) :]
+
     enc = tiktoken.get_encoding("gpt2")
     train_ids = enc.encode_ordinary(train_data)
     val_ids = enc.encode_ordinary(val_data)
+    print(f"Train data length: {len(train_data)}")
+    print(f"Validation data length: {len(val_data)}")
 
-    print(f"Train data: {len(train_ids)}")
-    print(f"Val data: {len(val_ids)}")
-
-    # Export to bin files
     train_ids = np.array(train_ids, dtype=np.uint16)
     val_ids = np.array(val_ids, dtype=np.uint16)
 
-    train_ids.tofile(os.path.join(os.path.dirname(__file__), "shakespeare-train.bin"))
-    val_ids.tofile(os.path.join(os.path.dirname(__file__), "shakespeare-val.bin"))
+    train_ids.tofile(os.path.join(os.path.dirname(__file__), "shakespeare_train.bin"))
+    val_ids.tofile(os.path.join(os.path.dirname(__file__), "shakespeare_val.bin"))
+    print("Shakespeare dataset tokenized and saved.")
 
-    print("Data saved to bin files")
+
+def get_shakespeare_data():
+    """
+    Returns the Shakespeare dataset as a tuple of (train_ids, val_ids).
+    """
+    if not os.path.exists(
+        os.path.join(os.path.dirname(__file__), "shakespeare_train.bin")
+    ) or not os.path.exists(
+        os.path.join(os.path.dirname(__file__), "shakespeare_val.bin")
+    ):
+        download_and_tokenize_shakespeare()
+
+    train_ids = np.fromfile(
+        os.path.join(os.path.dirname(__file__), "shakespeare_train.bin"),
+        dtype=np.uint16,
+    )
+    val_ids = np.fromfile(
+        os.path.join(os.path.dirname(__file__), "shakespeare_val.bin"), dtype=np.uint16
+    )
+    return train_ids, val_ids
 
 
 if __name__ == "__main__":
-    input_file_path = download_file()
-    load_and_tokenize(input_file_path)
+    download_and_tokenize_shakespeare()
