36d32c65455be935d75b1ea88b21cd67683b19f6
YuyangZhang <yuyangzhang2025@gmail.com>
2025-03-30 21:38:43 +0800

    change file name for convient

--- data/shakespear/prepare-shakespeare.py
+++ data/shakespear/prepare-shakespeare.py
@@ -43,18 +43,16 @@ def get_shakespeare_data():
     Returns the Shakespeare dataset as a tuple of (train_ids, val_ids).
     """
     if not os.path.exists(
-        os.path.join(os.path.dirname(__file__), "shakespeare_train.bin")
-    ) or not os.path.exists(
-        os.path.join(os.path.dirname(__file__), "shakespeare_val.bin")
-    ):
+        os.path.join(os.path.dirname(__file__), "train.bin")
+    ) or not os.path.exists(os.path.join(os.path.dirname(__file__), "val.bin")):
         download_and_tokenize_shakespeare()
 
     train_ids = np.fromfile(
-        os.path.join(os.path.dirname(__file__), "shakespeare_train.bin"),
+        os.path.join(os.path.dirname(__file__), "train.bin"),
         dtype=np.uint16,
     )
     val_ids = np.fromfile(
-        os.path.join(os.path.dirname(__file__), "shakespeare_val.bin"), dtype=np.uint16
+        os.path.join(os.path.dirname(__file__), "val.bin"), dtype=np.uint16
     )
     return train_ids, val_ids
 
